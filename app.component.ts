fbde209a45e5701bf01308ebc63a3069d637ad52
Yolanda <38848096+anne63@users.noreply.github.com>
2018-05-11 22:19:31 -0700

    initial commit

---
+++ app.component.ts
@@ -0,0 +1,21 @@
+//import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item'
+//import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+//import { Button } from 'protractor';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1 class="app-title">
+    Welcome to {{ title }}!
+  </h1>
+
+  <app-list-manager></app-list-manager>
+`,
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'My To Do List APP';
+}
